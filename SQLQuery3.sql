4fcc775210e97894ab7d46895c303c4d04670f51
HHDVasishtPranavUdathu <HariHaraDurgaVasishtPranav.Udathu@cognizant.com>
2025-03-18 09:10:22 +0530

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,3 +0,0 @@
-﻿DECLARE @IsLeaseConfirmed BIT;
-EXEC CheckLeaseStatus @Tenant_Id = 'T_102', @PropertyId = 1, @IsLeaseConfirmed = @IsLeaseConfirmed OUTPUT;
-SELECT @IsLeaseConfirmed AS IsLeaseConfirmed;
