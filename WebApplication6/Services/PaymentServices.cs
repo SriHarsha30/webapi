8b26fbe83d2a12994567cdcad1f8f1e0b7f96228
RonaldSudheer <Nakka.Sudheer@cognizant.com>
2025-03-18 14:30:44 +0530

    IAMBATMAN6

---
+++ WebApplication6/Services/PaymentServices.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using WebApplication6.Models;
+
+namespace WebApplication6.Services
+{
+    public class PaymentServices : IPaymentService
+    {
+        private readonly Context _context;
+
+        public PaymentServices(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Payment>> GetPaymentsByTenantIdAsync(string tenantId)
+        {
+            var commandText = "EXEC GetPaymentsByTenantId @Tenant_Id";
+            var tenantParam = new SqlParameter("@Tenant_Id", tenantId);
+
+            return await _context.Payments
+                                 .FromSqlRaw(commandText, tenantParam)
+                                 .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Payment>> GetPaymentsByOwnerid(string oid)
+        {
+            var commandText = "EXEC GetPaymentsByOwnerId @Owner_Id";
+            var tenantParam = new SqlParameter("@Owner_Id", oid);
+
+            return await _context.Payments
+                                 .FromSqlRaw(commandText, tenantParam)
+                                 .ToListAsync();
+        }
+
+        public async Task<bool> CheckTenantAndPropertyAsync(string tenantId, int propertyId)
+        {
+            var commandText = "EXEC CheckTenantAndProperty @Tenant_Id, @Property_Id";
+            var tenantParam = new SqlParameter("@Tenant_Id", tenantId);
+            var propertyParam = new SqlParameter("@Property_Id", propertyId);
+
+            try
+            {
+                await _context.Database.ExecuteSqlRawAsync(commandText, tenantParam, propertyParam);
+                return true; // If no error is raised, both tenant and property exist
+            }
+            catch (SqlException ex)
+            {
+                // Handle specific error messages if needed
+                if (ex.Message.Contains("Tenant does not exist"))
+                {
+                    return false;
+                }
+                else if (ex.Message.Contains("Property does not exist"))
+                {
+                    return false;
+                }
+                throw;
+            }
+        }
+    }
+}
