92b11ddf82be1fd07c0cb15848141e4c9ce6f449
HHDVasishtPranavUdathu <HariHaraDurgaVasishtPranav.Udathu@cognizant.com>
2025-03-19 11:30:41 +0530

    execp

---
+++ WebApplication6/Exceptions/InvalidDateRangeException.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication6.Exceptions
+{
+    public class InvalidDateRangeException : Exception
+    {
+        public InvalidDateRangeException()
+        : base("End date must be greater than start date.") { }
+
+        public InvalidDateRangeException(string message)
+            : base(message) { }
+
+        public InvalidDateRangeException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
