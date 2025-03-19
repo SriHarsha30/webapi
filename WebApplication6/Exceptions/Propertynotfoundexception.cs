f1a68ad20b450e8a19f39687a1a8786346a1a35e
SriHarsha30 <Petluri.Harsha@cognizant.com>
2025-03-19 12:12:46 +0530

    hgygfjyfghjy,kj

---
+++ WebApplication6/Exceptions/Propertynotfoundexception.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication6.Exceptions
+{
+    public class Propertynotfoundexception:Exception
+    {
+        
+            public Propertynotfoundexception()
+            : base("Requestued Property Not Found") { }
+
+            public Propertynotfoundexception(string message)
+                : base(message) { }
+
+            public Propertynotfoundexception(string message, Exception innerException)
+                : base(message, innerException) { }
+ 
+    }
+}
